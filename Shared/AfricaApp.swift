3ffbaa156927a99b3e8b82d3bb921a3c87e83539
Dzmitry M <ma.dzmitry@gmail.com>
2021-08-27 16:13:26 -0700

    Initial Commit

---
+++ Shared/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Shared
+//
+//  Created by Dzmitry Matsiulka on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
